7b4b89e12601eaae1dbe632d6eeb1745cde39dcf
William Gerald Blondel <contact@williamblondel.fr>
2017-04-01 20:16:45 +0200

    Print() refactoring

--- main.py
+++ main.py
@@ -29,20 +29,20 @@ while numAnnToScrap not in [item["numAnn"] for item in directories]:
     numAnnToScrap = input("Veuillez entrer le numéro du département dont vous voulez récupérer l'annuaire : ").zfill(3)
 
 dptToScrap = [item for item in directories if item['numAnn'] == numAnnToScrap][0]
-print("Vous avez choisi le département " + dptToScrap['nomDpt'] + " (" + dptToScrap['numAnn'] + ").")
+print("Vous avez choisi le département {} ({}).".format(dptToScrap["nomDpt"], dptToScrap["numAnn"]))
 print()
 
 if len(dptToScrap) > 2:
     print("Ce département a plusieurs annuaires.")
 
     for subAnn in dptToScrap["sousAnn"]:
-        print("> ", subAnn['nomAnn'] + " (" + subAnn['numAnn'] + ")")
+        print("> {} ({})".format(subAnn["nomAnn"], subAnn["numAnn"]))
 
     while numAnnToScrap not in [item["numAnn"] for item in dptToScrap["sousAnn"]]:
         numAnnToScrap = input("Lequel voulez-vous récupérer ? ")
 
     print()
-    print("Vous avez choisi l'annuaire", numAnnToScrap + ".")
+    print("Vous avez choisi l'annuaire {}.".format(numAnnToScrap))
 
 # We create the folder where the files will be downloaded
 Path(numAnnToScrap).mkdir(exist_ok=True)
@@ -52,8 +52,23 @@ for loop in tqdm(range(2, 700, 2), position=1):
     pageNumber = str(loop).zfill(4)
     pageNumberNext = str(loop+1).zfill(4)
 
-    url = "http://mesannuaires.pagesjaunes.fr/fsi/server?fext=%2Ejpg&source=%2Fpj%2F2016%2Fpja%2F" + numAnnToScrap + "%2Fp" + numAnnToScrap + pageNumber + "%5F0001%2Etif,%2Fpj%2F2016%2Fpja%2F" + numAnnToScrap + "%2Fp" + numAnnToScrap + pageNumberNext + "%5F0001%2Etif&effects=&disposition=true&save=1&profile=doublepage&rect=0%2C0%2C1%2C1&height=2000&width=2306&type=image&savename=2016%5FPJ%5F001%5F350%5F351"
-    saveas = Path(numAnnToScrap + "/" + "2016_PJ_001_" + pageNumber + "_" + pageNumberNext + ".jpg")
+    # {0} = numAnnToScrap
+    # {1} = pageNumber
+    # {2} = pageNumberNext
+    url = "http://mesannuaires.pagesjaunes.fr/fsi/server?" \
+          "fext=.jpg&" \
+          "source=/pj/2016/pja/{0}/p{0}{1}_0001.tif,/pj/2016/pja/{0}/p{0}{2}_0001.tif&" \
+          "effects=&" \
+          "disposition=true&" \
+          "save=1&" \
+          "profile=doublepage&" \
+          "rect=0,0,1,1&" \
+          "height=2000&" \
+          "width=2306&" \
+          "type=image&" \
+        .format(numAnnToScrap, pageNumber, pageNumberNext)
+
+    saveas = Path("{0}/2016_PJ_001_{1}_{2}.jpg".format(numAnnToScrap, pageNumber, pageNumberNext))
 
     # Streaming, so we can iterate over the response.
     r = requests.get(url, stream=True)
@@ -66,6 +81,6 @@ for loop in tqdm(range(2, 700, 2), position=1):
     with saveas.open(mode='wb') as f:
         pbar = tqdm(unit="B", total=int(r.headers['Content-Length']), unit_scale=True, leave=False, position=2)
         for chunk in r.iter_content(chunk_size=chunkSize):
-            if chunk: # filter out keep-alive new chunks
+            if chunk:  # filter out keep-alive new chunks
                 pbar.update(len(chunk))
                 f.write(chunk)
