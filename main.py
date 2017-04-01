76e898d0cff21a65ecf36436c4a96402c79e06ae
William Gerald Blondel <contact@williamblondel.fr>
2017-04-01 17:24:51 +0200

    Fixed: No such file or directory when folder does not exist

--- main.py
+++ main.py
@@ -3,20 +3,21 @@
 
 # PagesJaunesScrap Copyright (C) 2017    William Gerald Blondel
 # contact@williamblondel.fr
-# Last modified 1st April 2017 04.30am
+# Last modified 1st April 2017 05.24pm
 
 # TODO : Reformat the print()
 # TODO : Give the choice between PagesJaunes and PagesBlanches
 
 import json
+from pathlib import Path
 from tqdm import tqdm
 import requests
 
 
-DIRECTORIES_LIST_FILENAME = "liste_annuaires.json"
+DIRECTORIES_LIST_FILENAME = Path("liste_annuaires.json")
 
 # We load the list of directories
-with open(DIRECTORIES_LIST_FILENAME) as directories_list_fileobject:
+with DIRECTORIES_LIST_FILENAME.open() as directories_list_fileobject:
     directories = json.load(directories_list_fileobject)
 
 directories = directories["annuaires"]
@@ -43,13 +44,16 @@ if len(dptToScrap) > 2:
     print()
     print("Vous avez choisi l'annuaire", numAnnToScrap + ".")
 
+# We create the folder where the files will be downloaded
+Path(numAnnToScrap).mkdir(exist_ok=True)
+
 # We start to scrap
 for loop in tqdm(range(2, 700, 2), position=1):
     pageNumber = str(loop).zfill(4)
     pageNumberNext = str(loop+1).zfill(4)
 
     url = "http://mesannuaires.pagesjaunes.fr/fsi/server?fext=%2Ejpg&source=%2Fpj%2F2016%2Fpja%2F" + numAnnToScrap + "%2Fp" + numAnnToScrap + pageNumber + "%5F0001%2Etif,%2Fpj%2F2016%2Fpja%2F" + numAnnToScrap + "%2Fp" + numAnnToScrap + pageNumberNext + "%5F0001%2Etif&effects=&disposition=true&save=1&profile=doublepage&rect=0%2C0%2C1%2C1&height=2000&width=2306&type=image&savename=2016%5FPJ%5F001%5F350%5F351"
-    saveas = numAnnToScrap + "/" + "2016_PJ_001_" + pageNumber + "_" + pageNumberNext + ".jpg"
+    saveas = Path(numAnnToScrap + "/" + "2016_PJ_001_" + pageNumber + "_" + pageNumberNext + ".jpg")
 
     # Streaming, so we can iterate over the response.
     r = requests.get(url, stream=True)
@@ -59,9 +63,9 @@ for loop in tqdm(range(2, 700, 2), position=1):
 
     chunkSize = 1024
 
-    with open(saveas, 'wb') as f:
+    with saveas.open(mode='wb') as f:
         pbar = tqdm(unit="B", total=int(r.headers['Content-Length']), unit_scale=True, leave=False, position=2)
         for chunk in r.iter_content(chunk_size=chunkSize):
             if chunk: # filter out keep-alive new chunks
-                pbar.update (len(chunk))
+                pbar.update(len(chunk))
                 f.write(chunk)
